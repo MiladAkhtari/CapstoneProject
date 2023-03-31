e8583700eef115ae918e5ba09011f9ecf7af0c3f
milad <milad.akhtari@tekschool.us>
2023-03-30 20:32:49 -0700

    Final Update

--- src/test/java/tek/framework/runner/TestRunner.java
+++ src/test/java/tek/framework/runner/TestRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
 	@CucumberOptions(
-	        tags = "@testCase",
+	        tags = "@testCase11",
 	        features = ("classpath:features"),
 	        glue="tek.framework",
 	        dryRun = false,
